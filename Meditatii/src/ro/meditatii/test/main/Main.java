3d53beabd4237229de08b601e1c1c98839f052fa
Alec <spirescualex@gmail.com>
2014-03-13 16:36:35 +0200

    hhj

--- Meditatii/src/ro/meditatii/test/main/Main.java
+++ Meditatii/src/ro/meditatii/test/main/Main.java
@@ -1,12 +1,29 @@
 package ro.meditatii.test.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("YOLO");
-		System.out.println("Me too");
-		System.out.println("ers");
+		List strings = new ArrayList<String>();
+		
+		strings.add("Hello, ");
+		strings.add("Dave!");
+		String greeting = (String)strings.get(0) + (String)strings.get(1);
+		System.out.println(greeting);
+		
+		Integer e = new Integer(3);
+		
+		List<Number> numere = new ArrayList<>();
+		numere.add(3);
+		numere.add(3.14d);
+		System.out.println(numere);
+		
+		List<Integer> intregi = new ArrayList<>();
+		List<? extends Number> num2 = new ArrayList<>();
+		num2 = intregi;
+		
 	}
 
 }
